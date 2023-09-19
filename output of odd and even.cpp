90c2019ae8fad80a8e52a758dec18f29c832a928
shinadeveloper <145498719+shinadeveloper@users.noreply.github.com>
2023-09-20 01:24:25 +0530

    Add files via upload

---
+++ output of odd and even.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <math.h>
+#include <bits.h>
+using namespace std;
+
+//1st question
+// int main()
+// {
+// int array[5]={1,2,3,4,5};
+// int odd=0;
+// int even=0;
+// for (int i=0;i<5;i++){
+//     if((array[i]%2) ==0){
+//         even++;
+//     }
+//     else if((array[i]%2) !=0)
+//     {
+//         odd++;
+//     }
+// }
+// cout<<even<<"  "<<odd;
+// }
+ 
+//  //2nd question
+//  int main()
+//  {
+//      int n;
+//     cin>>n;
+//    int array[n];
+//    for(int i=0;i<n;i++){
+//     cin>>array[i];
+//    } 
+//    int  maxelement=array[0];
+//    for (int i=0;i<n;i++)
+//    {
+//     if (maxelement<array[i+1])
+//     {
+//         maxelement=array[i+1];
+//     }
+//     }
+//    int min=array[0];
+//    for (int i=0;i<n;i++)
+//    {
+//     if (min>array[i+1])
+//     {
+//         min=array[i+1];
+//     }
+//    cout<<maxelement+min<<endl;
+//  }
+
+// //  3rd question
+
+// int main()
+// {
+//    int N;
+//    cin>>N;
+//    int array[N];
+//    for(int i=0;i<N;i++){
+//     cin>>array[i];
+//    }
+//    for(int i=N-1;i>=0;i--){
+//      cout<< array[i];
+//    }
+
+
+
+// }
