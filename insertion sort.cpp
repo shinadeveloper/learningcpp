1704caf308c53875b6a8956845d8f9bda4246114
Dilraj Singh <145498719+shinadeveloper@users.noreply.github.com>
2023-12-15 16:15:08 +0530

    new files

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 void insertionsort(vector <int> &v){
-    int n=v.size();
-    
+    int n=v.size(); 
     for(int i=0;i<n;i++){
         int j=i;
         while(j>=0 && v[j-1]>v[j]){
             j--;
             swap(v[j+1],v[j]);
-
-        }     
-    }
-}
+        }}}
 int main(){
     int n;
     cin>>n;
     vector <int>array1(n);
     for(int i=0;i<n;i++){
-        cin>>array1[i];
-    }
+        cin>>array1[i];}
     insertionsort(array1);
     for(int i=0;i<n;i++){
         cout<<array1[i];
-    }
-}
+    }}
